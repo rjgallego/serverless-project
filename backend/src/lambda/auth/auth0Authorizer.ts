748e1787c2a6beaec1629e3b2db482a1316dea34
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-04-24 20:38:28 -0700

    Adding function to create certificat from jwks url and validate JWT token

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,7 +1,7 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
-import { verify, decode } from 'jsonwebtoken'
+import { verify, decode, JsonWebTokenError } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
 import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
@@ -12,7 +12,7 @@ const logger = createLogger('auth')
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = 'https://dev-jor6ny5m.us.auth0.com/.well-known/jwks.json'
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -57,11 +57,21 @@ export const handler = async (
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
-
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  const header = jwt.header;
+
+  if(header.alg !== 'RS256'){
+    throw new JsonWebTokenError('Only RS256 algorithm supported')
+  }
+
+  const cert = await getCertificate(jwksUrl)
+  
+
+  verify(token, cert, {algorithms: ['RS256']} ) as JwtPayload
+
+ return undefined
 }
 
 function getToken(authHeader: string): string {
@@ -74,4 +84,15 @@ function getToken(authHeader: string): string {
   const token = split[1]
 
   return token
+}
+
+async function getCertificate(jwksUrl: string){
+  try{
+    const response = await Axios.get(jwksUrl);
+    const key = response['data']['keys'][0]['x5c'][0];
+    const cert = `-----BEGIN CERTIFICATE-----\n${key}\n-----END CERTIFICATE-----\n`
+    return cert
+  }catch(error){
+    logger.error('Getting certificate failed', error)
+  }
 }
