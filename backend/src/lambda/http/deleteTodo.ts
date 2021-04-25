efe06e4105b4e15e2df3c33d4736f4c67ef19616
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-04-24 20:39:09 -0700

    Implementing delete todo function

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -2,9 +2,43 @@ import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
+import {getUserId} from '../utils';
+import * as AWS from 'aws-sdk'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+const todosTable = process.env.TODOS_TABLE
+
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
 
   // TODO: Remove a TODO item by id
-  return undefined
+  const userId = getUserId(event)
+  try{
+    await docClient.delete({
+      TableName: todosTable,
+      Key: {
+        userId: userId,
+        todoId: todoId
+      }
+    }).promise()
+  
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: ''
+    }
+  } catch (e){
+    return {
+      statusCode: 400,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        error: `Error deleting record: ${e}`
+      })
+    }
+  }
 }
