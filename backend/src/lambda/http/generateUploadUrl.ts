010e123d5659f985f3dcb8d9e1b7c6fdcf75b4e6
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-04-24 20:40:01 -0700

    implementing function to get upload URL for S3

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -2,9 +2,32 @@ import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
+import * as AWS from 'aws-sdk';
+
+const s3 = new AWS.S3({
+  signatureVersion: 'v4'
+})
+
+const bucketName = process.env.ATTACH_S3_BUCKET
+const urlExpiration = process.env.SIGNED_URL_EXPIRATION
+
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
+  const url = getUploadURL(todoId)
 
   // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      uploadURL: url
+    })
+  }
+}
+
+function getUploadURL(imageId: string){
+  return s3.getSignedUrl('putObject', {
+    Bucket: bucketName,
+    Key: imageId,
+    Expires: urlExpiration
+  })
 }
