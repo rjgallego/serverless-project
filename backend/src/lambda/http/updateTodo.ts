03fa7f0d6936e9cdaf18fd0be48b151187b3cb93
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-04-24 20:39:21 -0700

    Implementing update todo function

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -4,10 +4,60 @@ import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } f
 
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
 
+import * as AWS from 'aws-sdk';
+import { getUserId } from '../utils';
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
+const indexName = process.env.INDEX_NAME
+
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
+  const userId = getUserId(event)
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
 
   // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
+
+  const result = await docClient.query({
+    TableName: todosTable,
+    IndexName: indexName,
+    KeyConditionExpression: 'userId = :userId',
+    ExpressionAttributeValues: {
+      ':userId': userId
+    }
+  }).promise()
+
+  if(result.Count !== 0){
+    const result = await docClient.update({
+      TableName: todosTable,
+      Key: {
+        userId: userId,
+        todoId: todoId
+      },
+      ExpressionAttributeValues:{
+        ':name': updatedTodo.name,
+        ':dueDate': updatedTodo.dueDate,
+        ':done': updatedTodo.done
+      },
+      UpdateExpression: 'SET name = :name, dueDate = :dueDate, done = :done',
+      ReturnValues: 'ALL_NEW'
+    }).promise();
+
+    const attributes = result.Attributes;
+
+    return {
+      statusCode: 204,
+      body: JSON.stringify({
+        attributes
+      })
+    };
+  }
+
+  return {
+    statusCode: 404,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: ''
+  }
 }
