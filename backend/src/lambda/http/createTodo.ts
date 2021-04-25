5d74d728416190f5cf8976c3d1b3b8716f302054
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-04-24 20:38:52 -0700

    Implementing create todo function

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -4,9 +4,35 @@ import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } f
 
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 
+import * as uuid from 'uuid';
+import * as AWS from 'aws-sdk';
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
+
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
+  const itemId = uuid.v4()
+  const createdAt = new Date().toISOString()
 
+  const newItem = {
+    id: itemId,
+    createdAt: createdAt,
+    ...newTodo
+  }
   // TODO: Implement creating a new TODO item
-  return undefined
+  await docClient.put({
+    TableName: todosTable,
+    Item: newItem
+  }).promise()
+
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      newItem
+    })
+  }
 }
