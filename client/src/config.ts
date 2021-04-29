412927a5c7fe20406285e80bdf26c8a95bb912e8
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-04-28 17:36:36 -0700

    Adding apiId, domain, and clientId

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = 'n00c10rlqf'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-jor6ny5m.us.auth0.com',            // Auth0 domain
+  clientId: 'bvapJQaqRSaCWqwwdWaskSJSwnFHGpWh',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
